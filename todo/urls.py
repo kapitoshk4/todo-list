b2a146e614a20eb6c77fd7678d8007f130874c3c
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-18 23:15:58 +0300

    Add TagCreateView to views.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from todo.views import TodoListView, TagListView
+from todo.views import TodoListView, TagListView, TagCreateView
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 app_name = "todo"
