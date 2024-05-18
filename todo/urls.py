857046f9507707a1c642c2b47e552402758062d9
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-18 23:59:33 +0300

    Add tags/pk/delete/ url in urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from todo.views import TodoListView, TagListView, TagCreateView, TagUpdateView
+from todo.views import TodoListView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update")
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete")
 ]
 
 app_name = "todo"
