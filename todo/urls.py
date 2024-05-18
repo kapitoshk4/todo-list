169ccbc6f1bb2ea8f6f1771ae7bf03bec3e92cb4
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-18 22:54:48 +0300

    Add tags/ url in urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from todo.views import TodoListView
+from todo.views import TodoListView, TagListView
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="task-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo"
