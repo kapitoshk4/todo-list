699b52ab5633e09e29889bc8b5982681b46b3756
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-18 21:47:39 +0300

    Add TodoListView

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Todo
+
+
+class TodoListView(generic.ListView):
+    model = Todo
+    template_name = "todo/todo_list.html"
+    context_object_name = "todo-list"
