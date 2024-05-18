8857e09f604dfaeafb8c65faac502ec76ee5e318
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-19 00:49:45 +0300

    Add tasks/pk/delete/ url

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from todo.views import TodoListView, TagListView, TagCreateView, TagUpdateView, TagDeleteView, TodoCreateView, \
-    TodoUpdateView
+    TodoUpdateView, TodoDeleteView
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="task-list"),
@@ -9,7 +9,8 @@ urlpatterns = [
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
     path("tasks/create/", TodoCreateView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/update/", TodoUpdateView.as_view(), name="task-update")
+    path("tasks/<int:pk>/update/", TodoUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TodoDeleteView.as_view(), name="task-delete")
 ]
 
 app_name = "todo"
