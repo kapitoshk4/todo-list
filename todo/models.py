7eef4fd66c945e7300a4c44bc369e07cbeeb9472
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-18 21:02:52 +0300

    Create models.py & add Tag, Task modules

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.title
+
+
+class Todo(models.Model):
+    DONE = "Done"
+    NOT_DONE = "Not Done"
+
+    STATUS_CHOICES = [
+        (DONE, "Done"),
+        (NOT_DONE, "Not Done")
+    ]
+
+    title = models.CharField(max_length=60)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=NOT_DONE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="todos", blank=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.title} - {self.status}"
