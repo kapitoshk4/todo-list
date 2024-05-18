6d47253a2ea71efc7a94cdffdfc73e6919d20ed0
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-19 00:39:06 +0300

    Add tasks/create/ url

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from todo.views import TodoListView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
+from todo.views import TodoListView, TagListView, TagCreateView, TagUpdateView, TagDeleteView, TodoCreateView
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete")
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tasks/create/", TodoCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "todo"
