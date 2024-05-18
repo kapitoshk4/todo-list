045f6e081788542b3945a5edebc6703d6d48eb0f
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-19 00:37:04 +0300

    Add TodoForm

--- todo/forms.py
+++ todo/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 
-from todo.models import Tag
+from todo.models import Tag, Todo
 
 
 class TagForm(forms.ModelForm):
     class Meta:
         model = Tag
         fields = ["title", ]
+
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ["title", "deadline", "tags"]
+
+    deadline = forms.DateField(
+        widget=forms.DateInput(attrs={
+            "type": "date"
+        })
+    )
