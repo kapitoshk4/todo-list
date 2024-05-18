805798c89a10baff3d928041c15306bf5e126429
Dmytro Shchoma <dmytroshchoma@gmail.com>
2024-05-18 23:42:14 +0300

    Add url tags/pk/update/ to urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from todo.views import TodoListView, TagListView, TagCreateView
+from todo.views import TodoListView, TagListView, TagCreateView, TagUpdateView
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update")
 ]
 
 app_name = "todo"
